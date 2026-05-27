ec269fd83895e02783f82c582472c79854daf6d1
ReizeXD <andersonreize@gmail.com>
2026-05-27 14:56:43 -0300

    feat: add list roles and assign roles by admin

--- src/main/java/com/ufal/smartagro/domain/model/enums/Role.java
+++ src/main/java/com/ufal/smartagro/domain/model/enums/Role.java
@@ -1,8 +1,19 @@
 package com.ufal.smartagro.domain.model.enums;
 
 public enum Role {
-    ADMIN,
-    MANAGER,
-    TECHNICIAN,
-    PRODUCER
+    ADMIN("Administrador do sistema"),
+    MANAGER("Gestor de cooperativa/associação"),
+    TECHNICIAN("Técnico agrícola"),
+    PRODUCER("Agricultor/Produtor rural");
+
+    private final String description;
+
+    Role(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
+    
